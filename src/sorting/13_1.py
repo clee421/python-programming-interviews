09a42f5105bbbc67f266650a13b83e625742a006
Calvin Lee <clee421@gmail.com>
2025-06-13 22:00:22 -0400

    feat: 13_1 how is this even a q

--- src/sorting/13_1.py
+++ src/sorting/13_1.py
@@ -3,8 +3,19 @@ Compute the Intersection of Two Sorted Arrays
 """
 
 
-def solve() -> int:
-    return 0
+def solve(list_1: list[int], list_2: list[int]) -> list[int]:
+    res_set = set()
+    set_1 = set(list_1)
+    for n in list_2:
+        if n in set_1:
+            res_set.add(n)
+
+    return sorted(list(res_set))
 
 
-print(solve())
+print(
+    solve(
+        [5, 5, 6, 8, 8, 9, 10, 10],
+        [2, 3, 3, 5, 5, 6, 7, 7, 8, 12],
+    )
+)
