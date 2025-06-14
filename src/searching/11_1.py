50fcb3c45a074464119652149d5b40e0d4276519
Calvin Lee <clee421@gmail.com>
2025-06-13 21:25:41 -0400

    feat: 11_1 binary search

--- src/searching/11_1.py
+++ src/searching/11_1.py
@@ -3,8 +3,23 @@ Search a Sorted Array for First Occurrance of k
 """
 
 
-def solve() -> int:
-    return 0
+def solve(_list: list[int], target: int) -> int:
+    s, e = 0, len(_list) - 1
+    while s <= e:
+        mid = int((s + e) / 2)
+        if _list[mid] == target:
+            return mid
+
+        if target < _list[mid]:
+            e = mid - 1
+        else:
+            s = mid + 1
+
+    return -1
 
 
-print(solve())
+print(solve([-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], 8))
+print(solve([-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], 243))
+print(solve([-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], 401))
+print(solve([-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], -14))
+print(solve([-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], 108))
