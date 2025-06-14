35c8befbe1b85ba5386ce8e41ae09903013ecfab
Calvin Lee <clee421@gmail.com>
2025-06-13 21:50:00 -0400

    feat: 12_1 easy q

--- src/hash_tables/12_1.py
+++ src/hash_tables/12_1.py
@@ -3,8 +3,22 @@ Test for Palindromic Permutations
 """
 
 
-def solve() -> int:
-    return 0
+def solve(word_1: str, word_2: str) -> bool:
+    c_dict: dict[str, int] = {}
+    for c in word_1:
+        v = c_dict.get(c, 0)
+        c_dict[c] = v + 1
+
+    for c in word_2:
+        if c not in c_dict:
+            return False
+
+        c_dict[c] -= 1
+
+    return not any(c_dict.values())
 
 
-print(solve())
+print(solve("edified", "deified"))
+print(solve("edified", "deifid"))
+print(solve("edifid", "deified"))
+print(solve("abc", "efg"))
