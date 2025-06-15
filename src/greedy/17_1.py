2fd229a5a98d21166917d3bacb94dc37d424d870
Calvin Lee <clee421@gmail.com>
2025-06-14 20:12:45 -0400

    feat: 18_1 too easy

--- src/greedy/17_1.py
+++ src/greedy/17_1.py
@@ -3,8 +3,17 @@ Compute an Optimum Assignment of Tasks
 """
 
 
-def solve() -> int:
-    return 0
+def solve(tasks: list[int]) -> int:
+    sorted_tasks = sorted(tasks)
+
+    longest = 0
+    s, e = 0, len(sorted_tasks) - 1
+    while s <= e:
+        longest = max(longest, sorted_tasks[s] + sorted_tasks[e])
+        s += 1
+        e -= 1
+
+    return longest
 
 
-print(solve())
+print(solve([5, 2, 1, 6, 4, 4]))
